23305034ae097cdaa580df2f64c98a3fe70088b7
Andonovski Anja <131977707+AndonovskiA@users.noreply.github.com>
2023-06-17 15:23:19 +0200

    Pocetak_rada

---
+++ DZ1Csharp.cs
@@ -0,0 +1,15 @@
+﻿
+//// DZ
+// Za uneseni dvoznamenkasti broj
+// ispišite jediničnu vrijednost
+// unos 21, ispis 1
+// unos 87, ispis 7
+
+int ab = 21;
+Console.WriteLine(ab%10);
+
+int bc = 87;
+Console.WriteLine(87%10);
+
+
+
